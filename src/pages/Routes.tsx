161587a72f270609e2d79a42ef2d595d0c9b9696
Desenvo <azgadebr@gmail.com>
2026-02-25 17:17:36 -0300

    Criar Routes.tsx

---
+++ src/pages/Routes.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Routes() {
+  return (
+    <div className="card">
+      <h3>Rotas</h3>
+      <p className="muted">Página de rotas carregou ✅</p>
+    </div>
+  );
+}
