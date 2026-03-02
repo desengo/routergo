3ff17468198ba8f90a10b65957e20353d30b252b
Desenvo <azgadebr@gmail.com>
2026-03-02 15:06:52 -0300

    Atualizar o Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -13,7 +13,7 @@ export default function Dashboard() {
       <div className="topbar">
         <div className="brand">
           <img
-            src="https://i.ibb.co/GfhqSrxm/file-00000000a9c871f589252b63d66b7839.png"
+          src="https://i.ibb.co/DPYsRh9r/file-00000000a9c871f589252b63d66b7839-removebg-preview.png"
             alt="RouterGo"
             className="brandLogo"
           />
