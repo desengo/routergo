bd732eae9013a7befb75fae312c766e345d42f9b
Desenvo <azgadebr@gmail.com>
2026-03-02 14:53:01 -0300

    Atualizar o Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -39,7 +39,7 @@ export default function Login() {
       <div className="card" style={{ maxWidth: 520, margin: "60px auto" }}>
         <div className="loginHeader">
           <img  
-  src="https://i.ibb.co/DPYsRh9r/file-00000000a9c871f589252b63d66b7839-removebg-preview.png"
+  src="https://i.ibb.co/DPYsRh9r/file-00000000a9c871f589252b63d66b7839-removebg-preview.png"/>
 
           <alt="RouterGo"
            className="loginLogo"
