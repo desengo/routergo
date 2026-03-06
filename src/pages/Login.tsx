8ae31ddec09788d4037b24541b8b9dcd2e3c541f
Desenvo <azgadebr@gmail.com>
2026-03-06 18:05:10 -0300

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -112,12 +112,7 @@ export default function Login() {
       {screen === "login" && (
         <div className="card">
           <div className="row" style={{ gap: 10, alignItems: "center" }}>
-            <img
-              src="https://i.ibb.co/DPYsRh9r/file-00000000a9c871f589252b63d66b7839-removebg-preview.png"
-              alt="RouterGo"
-              style={{ width: 36, height: 36 }}
-            />
-            <div>
+           
               <div style={{ fontSize: 28, fontWeight: 900 }}>RouterGo</div>
               <div className="muted">Gestão inteligente de entregas</div>
             </div>
